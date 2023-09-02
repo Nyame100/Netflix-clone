53810c4018e3fe24feaa1ba790db5377dddd54d3
nyame100 <nyamerichmond100@gmail.com>
2023-09-02 22:47:11 +0200

    Add billboard component
    This updates adds a billboard component which loads a random movie
    from the database whenever the homepage is loaded.

---
+++ hooks/useBillboard.ts
@@ -0,0 +1,14 @@
+import useSWR from "swr";
+
+import fetcher from "@/lib/fetcher";
+
+const useBillboard = () => {
+  const { data, isLoading, error } = useSWR("/api/random", fetcher, {
+    revalidateIfStale: false,
+    revalidateOnFocus: false,
+    revalidateOnReconnect: false,
+  });
+  return { data, error, isLoading };
+};
+
+export default useBillboard;
