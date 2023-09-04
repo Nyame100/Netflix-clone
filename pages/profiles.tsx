7398543d7e0113a2d2194fd314079bc9eff251c9
nyame100 <nyamerichmond100@gmail.com>
2023-09-04 21:20:53 +0200

    bug fix

--- pages/profiles.tsx
+++ pages/profiles.tsx
@@ -2,6 +2,7 @@ import useCurrentUser from "@/hooks/useCurrentUser";
 import { NextPageContext } from "next";
 import { getSession } from "next-auth/react";
 import { useRouter } from "next/router";
+import { useCallback } from "react";
 
 export async function getServerSideProps(context: NextPageContext) {
   const session = await getSession(context);
@@ -23,6 +24,10 @@ const Profiles = () => {
   const router = useRouter();
   const { data: user } = useCurrentUser();
 
+  const selectProfile = useCallback(() => {
+    router.push("/");
+  }, [router]);
+
   return (
     <div className="flex items-center h-full justify-center">
       <div className="flex flex-col">
@@ -30,7 +35,7 @@ const Profiles = () => {
           Who is watching
         </h1>
         <div className="flex items-center justify-center gap-8 mt-10">
-          <div onClick={() => router.push("/")}>
+          <div onClick={() => selectProfile()}>
             <div className="group flex-row w-44 mx-auto">
               <div
                 className="w-44 h-44 
