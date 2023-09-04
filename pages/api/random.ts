962c8841228c73d9e9439dabc14d2f12ed60571e
nyame100 <nyamerichmond100@gmail.com>
2023-09-04 21:44:51 +0200

    Error fix

--- pages/api/random.ts
+++ pages/api/random.ts
@@ -7,10 +7,10 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  if (req.method !== "GET") {
-    return res.status(405).end();
-  }
   try {
+    if (req.method !== "GET") {
+      return res.status(405).end();
+    }
     await serverAuth(req, res);
 
     const movieCount = await prismadb.movie.count();
@@ -24,6 +24,6 @@ export default async function handler(
     return res.status(200).json(randomMovies[0]);
   } catch (error) {
     console.log(error);
-    return res.status(400).end();
+    return res.status(500).end();
   }
 }
